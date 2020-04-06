7e88d1b500e736ce22da235e12ada24f582a2d27
Charles Pelton <charliejp0311@gmail.com>
2020-04-06 17:03:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+  get "/name" do
+    "Charlie Pelton"
+  end
+  get "/hometown" do
+    "DeKalb, IL"
+  end
+  get "/favorite-song" do
+    "Immigrant Song - Led Zepplin"
+  end
 end
